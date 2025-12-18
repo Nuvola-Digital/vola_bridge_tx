c624d95f526d05b6a479f9fc6657d0ce5bb0c06d
piyush thapa <piyush.codelabs@gmail.com>
2025-12-18 08:24:17 +0545

    fix: fetch sutra package  from github

--- mix.exs
+++ mix.exs
@@ -28,8 +28,7 @@ defmodule VolaBridgeTx.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      # {:sutra, git: "https://github.com/txbody-org/sutra-cardano"}
-      {:sutra, path: "/home/piyush/projects/txbody/sutra-sdk"}
+      {:sutra, git: "https://github.com/txbody-org/sutra-cardano"}
     ]
   end
 end

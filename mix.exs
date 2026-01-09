629d8a3d0ecdb46e5dac85f42fbf5174390183b8
Piyush <53209021+piyushthapa@users.noreply.github.com>
2026-01-09 13:04:12 +0545

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule VolaBridgeTx.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:sutra, git: "https://github.com/txbody-org/sutra-cardano"}
+      {:sutra, "~> 0.2.4", hex: :sutra_cardano}
     ]
   end
 end
